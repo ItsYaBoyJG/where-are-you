e697866d99d04d54728876d61e57af974f705297
Justin <goshornjustin@gmail.com>
2025-06-25 12:14:40 -0400

    second commit

---
+++ where-are-you/Screens/friends.swift
@@ -0,0 +1,8 @@
+//
+//  friends.swift
+//  where-are-you
+//
+//  Created by Justin Goshorn on 5/14/25.
+//
+
+import Foundation
---
+++ where-are-you/Utils/location.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  where-are-you
+//
+//  Created by Justin Goshorn on 5/14/25.
+//
+
+import Foundation
